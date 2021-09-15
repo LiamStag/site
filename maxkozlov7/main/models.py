f6f1cb556cd170ae5301ffa30e3a20cbb0c3dc51
Maxim Gafiatulin <89521721+LiamStag@users.noreply.github.com>
2021-09-15 18:57:05 +0500

    add models

--- maxkozlov7/main/models.py
+++ maxkozlov7/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Works(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=true)
+    video = models.FileField(upload_to="video/%Y/%m/%d/")
+    time_create = models.DateTimeField(auto_now_add=true)
+    time_update = models.DateTimeField(auto_now=true)
+    is_published = models.BooleanField(default=True)
